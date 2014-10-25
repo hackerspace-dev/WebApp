e2a996062f9ba1c8627f524f3b9f9bca6583115c
animos3d <kasia@kasia-VAIO>
2014-10-25 12:31:00 +0200

    Hackerspace

--- src/pl/hackerspaceDev/webApp/view/HelloServlet.java
+++ src/pl/hackerspaceDev/webApp/view/HelloServlet.java
@@ -25,7 +25,7 @@ public class HelloServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Hello!");
+		System.out.println("Hello! Hackerspace");
 		response.getWriter().println("Hello!");
 		
 		
