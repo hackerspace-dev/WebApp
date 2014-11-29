7fe1f6df35ebb1298eee7a802ca8121c8302b556
Cichy3D <Cichy3D@xps.mshome.net>
2014-11-29 14:32:49 +0100

    JQuery tutorial, DOM (ok), AJAX liźnięty, REST liźnięty

---
+++ src/pl/hackerspaceDev/webApp/view/Rest.java
@@ -0,0 +1,62 @@
+package pl.hackerspaceDev.webApp.view;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Rest
+ */
+@WebServlet({ "/Rest", "/rest" })
+public class Rest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Rest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		String nazwa = request.getParameter("nazwa");
+		nazwa = new StringBuffer(nazwa).reverse().toString();
+		
+		String obj = "{\"nazwa\":\"%s\",\"val\":\"%d\"}";
+		obj = String.format(obj, nazwa, 123);
+		
+		response.getWriter().println(obj);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
