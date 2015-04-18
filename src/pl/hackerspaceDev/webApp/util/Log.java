29172f4c29d6440d5c77a0bf093b0fec9a2cbbdd
Cichy3D <Cichy3D@xps.mshome.net>
2015-04-18 13:09:22 +0200

    AngularJS + REST post

--- src/pl/hackerspaceDev/webApp/util/Log.java
+++ src/pl/hackerspaceDev/webApp/util/Log.java
@@ -9,100 +9,100 @@ import pl.hackerspaceDev.webApp.service.HelloBean;
 public class Log {
 
 	
-	public static void fatal(Class clazz, String msg, Throwable exception){
+	public static void fatal(Class clazz, Object msg, Throwable exception){
 		Logger logger = Logger.getLogger(clazz);
 		logger.fatal(msg, exception);
 	}
 	
-	public static void error(Class clazz, String msg, Throwable exception){
+	public static void error(Class clazz, Object msg, Throwable exception){
 		Logger logger = Logger.getLogger(clazz);
 		logger.error(msg, exception);
 	}
 	
-	public static void warn(Class clazz, String msg, Throwable exception){
+	public static void warn(Class clazz, Object msg, Throwable exception){
 		Logger logger = Logger.getLogger(clazz);
 		logger.warn(msg, exception);
 	}
 	
-	public static void fatal(Class clazz, String msg){
+	public static void fatal(Class clazz, Object msg){
 		Logger logger = Logger.getLogger(clazz);
 		logger.fatal(msg);
 	}
 	
-	public static void error(Class clazz, String msg){
+	public static void error(Class clazz, Object msg){
 		Logger logger = Logger.getLogger(clazz);
 		logger.error(msg);
 	}
 	
-	public static void warn(Class clazz, String msg){
+	public static void warn(Class clazz, Object msg){
 		Logger logger = Logger.getLogger(clazz);
 		logger.warn(msg);
 	}
 	
-	public static void info(Class clazz, String msg){
+	public static void info(Class clazz, Object msg){
 		Logger logger = Logger.getLogger(clazz);
 		logger.info(msg);
 	}
 	
-	public static void debug(Class clazz, String msg){
+	public static void debug(Class clazz, Object msg){
 		Logger logger = Logger.getLogger(clazz);
 		logger.debug(msg);
 	}
 	
-	public static void trace(Class clazz, String msg){
+	public static void trace(Class clazz, Object msg){
 		Logger logger = Logger.getLogger(clazz);
 		logger.trace(msg);
 	}
 	
-	public static void fatal(String msg, Throwable exception){
+	public static void fatal(Object msg, Throwable exception){
 		Class clazz = sun.reflect.Reflection.getCallerClass(2);
 		Logger logger = Logger.getLogger(clazz);
 		logger.fatal(msg, exception);
 	}
 	
-	public static void error(String msg, Throwable exception){
+	public static void error(Object msg, Throwable exception){
 		Class clazz = sun.reflect.Reflection.getCallerClass(2);
 		Logger logger = Logger.getLogger(clazz);
 		logger.error(msg, exception);
 	}
 	
-	public static void warn(String msg, Throwable exception){
+	public static void warn(Object msg, Throwable exception){
 		Class clazz = sun.reflect.Reflection.getCallerClass(2);
 		Logger logger = Logger.getLogger(clazz);
 		logger.warn(msg, exception);
 	}
 	
-	public static void fatal(String msg){
+	public static void fatal(Object msg){
 		Class clazz = sun.reflect.Reflection.getCallerClass(2);
 		Logger logger = Logger.getLogger(clazz);
 		logger.fatal(msg);
 	}
 	
-	public static void error(String msg){
+	public static void error(Object msg){
 		Class clazz = sun.reflect.Reflection.getCallerClass(2);
 		Logger logger = Logger.getLogger(clazz);
 		logger.error(msg);
 	}
 	
-	public static void warn(String msg){
+	public static void warn(Object msg){
 		Class clazz = sun.reflect.Reflection.getCallerClass(2);
 		Logger logger = Logger.getLogger(clazz);
 		logger.warn(msg);
 	}
 	
-	public static void info(String msg){
+	public static void info(Object msg){
 		Class clazz = sun.reflect.Reflection.getCallerClass(2);
 		Logger logger = Logger.getLogger(clazz);
 		logger.info(msg);
 	}
 	
-	public static void debug(String msg){
+	public static void debug(Object msg){
 		Class clazz = sun.reflect.Reflection.getCallerClass(2);
 		Logger logger = Logger.getLogger(clazz);
 		logger.debug(msg);
 	}
 	
-	public static void trace(String msg){
+	public static void trace(Object msg){
 		Class clazz = sun.reflect.Reflection.getCallerClass(2);
 		Logger logger = Logger.getLogger(clazz);
 		logger.trace(msg);
--- src/pl/hackerspaceDev/webApp/view/Rest.java
+++ src/pl/hackerspaceDev/webApp/view/Rest.java
@@ -1,12 +1,18 @@
 package pl.hackerspaceDev.webApp.view;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.text.ParseException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import pl.hackerspaceDev.webApp.util.Log;
 
 /**
  * Servlet implementation class Rest
@@ -36,13 +42,33 @@ public class Rest extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
-		String nazwa = request.getParameter("nazwa");
+		
+		Log.info(request.getParameterMap().keySet());
+		Log.info(request.getParameter("parametr"));
+		
+		JSONObject jsonObject = extractPostData(request);
+		
+		String nazwa = jsonObject.getString("nazwa");
 		nazwa = new StringBuffer(nazwa).reverse().toString();
+		jsonObject.put("nazwa", nazwa);
+		jsonObject.put("val", 1234);
 		
-		String obj = "{\"nazwa\":\"%s\",\"val\":\"%d\"}";
-		obj = String.format(obj, nazwa, 123);
-		
-		response.getWriter().println(obj);
+		Log.info(jsonObject);
+		try {
+			Thread.sleep(15000);
+		} catch (InterruptedException e) { }
+		response.getWriter().println(jsonObject);
+	}
+
+	private JSONObject extractPostData(HttpServletRequest request) throws IOException {
+		StringBuffer jb = new StringBuffer();
+		String line = null;
+	    BufferedReader reader = request.getReader();
+	    while ((line = reader.readLine()) != null){
+	    	jb.append(line);
+	    } 
+		JSONObject jsonObject = new JSONObject(jb.toString());
+		return jsonObject;
 	}
 
 	/**
